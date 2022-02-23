d81c5c1037b8f8cb3b5c4b2690793936e0ff991f
Taye Lemma <lemmataye@yahoo.com>
2022-02-23 00:26:42 -0600

    W1D1
    Assignment

---
+++ W1D1/forLoop.js
@@ -0,0 +1,31 @@
+"use strict"
+
+for (let i = 1; i <= 5; i++) {
+    let row = "";
+    for (let j = 1; j <= 5; j++) {
+        row += j;
+    }
+    console.log(row);
+}
+console.log();
+
+
+
+for (let i = 1; i <= 5; i++) {
+    let row = "";
+    for (let j = 1; j <= i; j++) {
+        row += i;
+    }
+    console.log(row);
+}
+console.log();
+
+
+
+for (let i = 5; i > 0; i--) {
+    let row = "";
+    for (let j = 1; j <= i; j++) {
+        row += i;
+    }
+    console.log(row);
+}
