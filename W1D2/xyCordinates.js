b1e2c4e7ac62fa435da4518c18f6ce88972b6289
Taye Lemma <lemmataye@yahoo.com>
2022-02-27 14:56:13 -0600

    done

--- W1D2/xyCordinates.js
+++ W1D2/xyCordinates.js
@@ -4,14 +4,14 @@ Question 6. The distance between two points.
 */
 /**
  * 
- * @param {int} x_1 the firsts number. 
- * @param {int} y_1 the second number.
- * @param {int} x_2 the third number.
- * @param {int} y_2 the fourth number.
+ * @param {int} xX1 the firsts number. 
+ * @param {int} yY1 the second number.
+ * @param {int} xX2 the third number.
+ * @param {int} yY2 the fourth number.
  * @returns {int} the distance between two points. 
  */
-function calcDistance(x_1,y_1,x_2,y_2){
-    let number = (Math.pow(x_2-x_1),2)+ (Math.pow(y_2-y_1),2);
+function calcDistance(xX1,yY1,xX2,yY2){
+    let number = Math.pow((xX2-xX1),2) + Math.pow((yY2-yY1),2);
     return Math.sqrt(number);
 }
 console.log("expect 7.07: ", calcDistance(0,0,5,5));
