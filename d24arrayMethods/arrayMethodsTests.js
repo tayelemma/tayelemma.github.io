ab24f8443b08e6acda3703e9932914476fb09749
Taye Lemma <lemmataye@yahoo.com>
2022-03-03 16:47:44 -0600

    done

--- d24arrayMethods/arrayMethodsTests.js
+++ d24arrayMethods/arrayMethodsTests.js
@@ -1,13 +1,13 @@
 "use strict";
 /* global assert    */
 /* comment out the node specific code when going to the browser*/
-const assert = require("assert");  //always need this with node
-const myExports = require("./methods.js");  //with node need the name of your file with your functions here
-const groupById = myExports.groupById;  //do this for all of the functions used in the Mocha tests
-const unique = myExports.unique;
-const filterRangeInPlace = myExports.filterRangeInPlace;
-const filterRange = myExports.filterRange;
-const Calculator = myExports.Calculator;
+// const assert = require("assert");  //always need this with node
+// const myExports = require("./methods.js");  //with node need the name of your file with your functions here
+// const groupById = myExports.groupById;  //do this for all of the functions used in the Mocha tests
+// const unique = myExports.unique;
+// const filterRangeInPlace = myExports.filterRangeInPlace;
+// const filterRange = myExports.filterRange;
+// const Calculator = myExports.Calculator;
 
 
 
--- d24arrayMethods/methods.js
+++ d24arrayMethods/methods.js
@@ -1,7 +1,7 @@
 "use strict"
 
 /* You need the module.exports when testing in node.  Comment it out when you send your file to the browser */
-module.exports = { groupById, unique, filterRangeInPlace, filterRange, Calculator }; //add all of your function names here that you need for the node mocha tests
+// module.exports = { groupById, unique, filterRangeInPlace, filterRange, Calculator }; //add all of your function names here that you need for the node mocha tests
 
 
 
