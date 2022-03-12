b6ca2344a9d40496aed20b02ffc7f45c59608f08
Taye Lemma <lemmataye@yahoo.com>
2022-03-12 16:46:53 -0600

    done

--- d36inheritance/consBankTests.js
+++ d36inheritance/consBankTests.js
@@ -3,9 +3,9 @@
 You need the assert and function name declarations to test in node.  
 Comment these out when you send it to the browser with the index.html mocha setup page. */
 
-const assert = require("assert");  //always need this with node
-const myExports = require("./bank.js");  //with node need the name of your file with your functions here
-const MakeBank = myExports.MakeBank;  //do this for all of the functions used in the Mocha tests
+// const assert = require("assert");  //always need this with node
+// const myExports = require("./bank.js");  //with node need the name of your file with your functions here
+// const MakeBank = myExports.MakeBank;  //do this for all of the functions used in the Mocha tests
 
 
 describe("bank tests", function () {
